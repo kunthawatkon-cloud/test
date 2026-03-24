291d35c16d54edf5d048c6afa2b2fec0ed20eaef
kunthawatkon-cloud <kunthawat.kon@spumail.net>
2026-03-24 10:46:18 +0700

    Delete L9_68092306/company_sys directory

--- L9_68092306/company_sys/services/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('products/', views.products, name='products'),
-    path('feedback/', views.feedback, name='feedback'),
-]
